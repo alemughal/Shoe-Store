6c51c186f8d6fb33fe12582cf57597ddb41ea488
alemughal <alimug38@gmail.com>
2021-04-01 21:58:42 +0500

    Adding Image in Home Page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../Home/Home.module.css';
+import HomePic from '../../Images/home-pic.png'
 
 
 function Home() {
   return (
     <div className={styles.container}>
+      <div className={styles.pic}>
+      </div>
+      <img src={HomePic} alt="Shoe-Pic" width="40%"/>
       <h2 className={styles.home} id={styles.heading}>NEW IN</h2>
       <h1 className={styles.head}>YEEZY BOOST <br /> SPLY-350</h1>
       <h2 className={styles.home}>Explore the new collections of sneaker</h2>
-      <button><Link to="/product" id={styles.hello}>Product</Link></button>
-    </div>
+      <button><Link to="/product" id={styles.hello}>Explore <br/> Now</Link></button>
+      </div>
   )
 }
 
